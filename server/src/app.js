e21650734a56c0f1813f375f2aa8829d7028b2e3
Het-Dhorajiya-0305 <hetdhorajiya03@gmail.com>
2026-05-23 02:46:28 +0530

    one bug fixed

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,7 @@ import meetingRouter from './routes/meetingRoute.js';
 
 const app = express()
 
-app.use(cors({
-    origin:process.env.FRONTEND_URL || "http://localhost:3000",                       
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
